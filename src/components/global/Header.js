3327e701d924b8d93d07d2477b02cb6c4fd4fcbc
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-08-30 17:57:16 +0300

    header done

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,99 +1,115 @@
 "use client";
-import React, { useState } from "react";
-import { Container } from "react-bootstrap";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-
+import React, { useState, useEffect } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import Image from "next/image";
+
+import { AiOutlineClose } from "react-icons/ai";
+import { BiMenu } from "react-icons/bi";
+
+import classes from "./Header.module.scss";
 
 import BtnComponent from "@/components/ui/BtnComponent";
 
 import logo from "@/img/Logo-WoB.png";
 
 const Header = () => {
-  const [expanded, setExpanded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-  const toggleNav = () => {
-    setExpanded(!expanded);
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
 
-  const handleNavLinkClick = () => {
-    if (expanded) {
-      toggleNav(); // Close the navbar
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 992 && menuOpen) {
+      setMenuOpen(false);
     }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
   };
+
+  const navLinks = [
+    {
+      href: "/",
+      text: "About",
+    },
+    {
+      href: "/",
+      text: "Membership",
+    },
+    {
+      href: "/",
+      text: "Honor Circle",
+    },
+    {
+      href: "/",
+      text: "Contacts",
+    },
+  ];
 
   return (
     <Navbar expand="lg" fixed="top" className="bg-lipstick">
       <Container>
         <Navbar.Brand href="/">
-          <Image src={logo} alt="logo" className={`img-fluid`} />
+          <Image
+            src={logo}
+            alt="logo"
+            className={`img-fluid`}
+            width={75}
+            height={75}
+          />
         </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="navbar"
-          onClick={toggleNav}
-          className="white"
-        />
-        <Navbar.Collapse
-          id="navbar"
-          className={
-            expanded
-              ? "show d-flex flex-col justify-content-between"
-              : "flex-row justify-content-between"
-          }
+        <div className={classes.menuControls}>
+          {menuOpen ? (
+            <BiMenu onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+        <div
+          // id="navbar"
+          className={`${classes.navbar} ${
+            menuOpen && size.width < 992 ? "hidden" : ""
+          }`}
         >
-          <Nav
-            className={
-              expanded
-                ? "d-flex flex-col justify-content-center align-items-center my-5"
-                : "d-flex flex-row"
-            }
-          >
-            <Nav.Link href="#home">
-              <p className="text white mb-0" onClick={handleNavLinkClick}>
-                About
-              </p>
-            </Nav.Link>
-            <Nav.Link href="#about">
-              <p className="text white" onClick={handleNavLinkClick}>
-                Membership
-              </p>
-            </Nav.Link>
-            <Nav.Link href="#services">
-              <p className="text white" onClick={handleNavLinkClick}>
-                Honor Circle
-              </p>
-            </Nav.Link>
-            <Nav.Link href="#contact">
-              <p className="text white" onClick={handleNavLinkClick}>
-                Contacts
-              </p>
-            </Nav.Link>
-          </Nav>
-          <div
-            className={
-              expanded
-                ? "d-flex flex-col justify-content-center align-items-center my-5"
-                : "d-flex flex-row"
-            }
-          >
+          <div className={classes.navbar__container}>
+            {navLinks.map((link, index) => (
+              <Nav.Link key={index} href={link.href}>
+                <p className={`text white mb-0 mx-2 `}>{link.text}</p>
+              </Nav.Link>
+            ))}
+          </div>
+          <div className={classes.navbar__container}>
             <BtnComponent
               link={"#"}
               text="Join Us"
               bg="bg-blue"
               color="white"
-              classes={expanded ? "my-3" : "mx-3"}
-              onClick={handleNavLinkClick}
+              classes={classes.navbar__container__btn}
+              onClick={menuToggleHandler}
             />
             <BtnComponent
               link={"#"}
               text="Members Login"
               bg="bg-white"
               color="lipstick"
-              onClick={handleNavLinkClick}
+              onClick={menuToggleHandler}
             />
           </div>
-        </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
