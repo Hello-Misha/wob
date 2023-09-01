782728f0109541a5535550c2948ae6f7c9fe4d0e
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-09-01 19:47:09 +0300

    join section

--- components/Home/JoinUs.js
+++ components/Home/JoinUs.js
@@ -2,12 +2,40 @@ import { useTranslation } from "next-i18next";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "next/image";
 
-import Title from "../ui/Title";
+import classes from "./#Home.module.scss";
+
+import BtnComponent from "../ui/BtnComponent";
 
 import join from "../../public/img/home/join.png";
 
 const JoinUs = () => {
-  return <div>JoinUs</div>;
+  const { t } = useTranslation("home");
+  return (
+    <section className={`${classes.bgFullWidth} my-5`}>
+      <Container className={classes.bgFullWidth__container}>
+        <Row className="d-flex justify-between align-items-center">
+          <Col xs="12" md="4">
+            <Image
+              src={join}
+              alt="Join Us"
+              className="img-fluid mb-3 overflow-hidden	"
+            />
+          </Col>
+          <Col xs="12" md="8" className="py-5 textContainer">
+            <h2 className="h2-title white mb-4">{t("joinUs.title")}</h2>
+            <BtnComponent
+              locale="home"
+              link="#"
+              btnType={null}
+              btnText="joinUs.btn"
+              bg="bg-blue"
+              color="white"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default JoinUs;
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,9 +1,8 @@
-import { Container, Row, Col } from "react-bootstrap";
-
 import Hero from "../Home/Hero";
 import Icons from "../Home/Icons";
 import Mission from "../Home/Mission";
 import Founders from "../Home/Founders";
+import JoinUs from "../Home/JoinUs";
 
 const Home = () => {
   return (
@@ -12,6 +11,7 @@ const Home = () => {
       <Icons />
       <Mission />
       <Founders />
+      <JoinUs />
     </main>
   );
 };
