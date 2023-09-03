cf30fd9e0b87394dcd1b46e14516a33a56a68d1e
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-09-03 17:08:31 +0300

    founders block

--- components/views/Mission.js
+++ components/views/Mission.js
@@ -1,9 +1,11 @@
 import Content from "../Mission/Content";
+import Founders from "../Mission/Founders";
 
 const Mission = () => {
   return (
     <main>
       <Content />
+      <Founders />
     </main>
   );
 };
