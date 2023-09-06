5d0dc97a6ab995a72dc60b073334c17d91cf8fae
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-09-06 11:26:56 +0300

    membership slider and button

--- components/Membership/Content.js
+++ components/Membership/Content.js
@@ -1,37 +1,45 @@
 import { useTranslation } from "next-i18next";
 import { Container, Row, Col } from "react-bootstrap";
+
 import Title from "../ui/Title";
 import CarouselComponent from "../ui/CarouselWithText";
+import Text from "../ui/Text";
 
-import imgage from "../../public/img/membership/Iryna-Rubis.png";
+import image from "../../public/img/membership/Iryna-Rubis.png";
 const Content = () => {
   const { t } = useTranslation("membership");
-  const imgArr = [imgage, imgage, imgage];
+  const imgArr = [image, image, image];
   return (
     <section>
       <Container>
         <Row className="mb-5">
           <Title locale="membership" text="title" hr={true} />
         </Row>
-        {/* background */}
+
         <Row className="mb-5">
           <Col xs="12">
             <h3 className="h3-title  lipstick  pb-3">{t("main.title")}</h3>
-            {t("main.text", { returnObjects: true }).map((item, index) => (
-              <p key={index} className="text blue pb-3">
-                {item}
-              </p>
-            ))}
-          </Col>
-          <Col xs="12">
-            <h3 className="h3-title  lipstick  pb-3">{t("members.title")}</h3>
-
-            <CarouselComponent
-              locale={"membership"}
-              imgArr={imgArr}
-              textContent={"members.carousel"}
+            <Text
+              locale="membership"
+              text="main.text"
+              link="#"
+              btnType={null}
+              btnText="main.btn"
+              bg="bg-lipstick"
+              color="white"
             />
           </Col>
+          <Row className="mb-5">
+            <Col xs="12">
+              <h3 className="h3-title  lipstick  mt-5">{t("members.title")}</h3>
+
+              <CarouselComponent
+                locale={"membership"}
+                imgArr={imgArr}
+                textContent={"members.carousel"}
+              />
+            </Col>
+          </Row>
         </Row>
       </Container>
     </section>
--- components/ui/CarouselWithText.js
+++ components/ui/CarouselWithText.js
@@ -8,18 +8,18 @@ import classes from "./#UI.module.scss";
 const CarouselComponent = ({ locale, imgArr, textContent }) => {
   const { t } = useTranslation(locale);
   return (
-    <Carousel>
+    <Carousel className="my-5">
       {t(textContent, { returnObjects: true }).map((item, index) => (
         <Carousel.Item key={index}>
           <Container>
             <Row className="gx-5">
-              <Col xs="12" md="3" className={`mx-auto ${classes.border_right}`}>
+              <Col xs="12" md="3" className={`mx-auto `}>
                 <Image
-                  className="img-fluid d-block "
+                  className="img-fluid d-block mx-auto"
                   src={imgArr[index]}
                   placeholder="blur"
-                  width={300}
-                  height={300}
+                  width={250}
+                  height={250}
                   alt="Slide"
                 />
 
@@ -30,10 +30,12 @@ const CarouselComponent = ({ locale, imgArr, textContent }) => {
                   {t(item.title)}
                 </p>
               </Col>
-              <Col xs="12" md="9">
-                <h2 className="h1-title text-left lipstick">“</h2>
-                <p className="text-italic blue pb-3">{t(item.quote)}</p>
-                <h2 className="h1-title text-right lipstick">”</h2>
+              <Col xs="12" md="9" className="">
+                <div className={`${classes.border}`}>
+                  <h2 className="h1-title text-left lipstick">“</h2>
+                  <p className="text-italic blue pb-3">{t(item.quote)}</p>
+                  <h2 className="h1-title text-right lipstick">”</h2>
+                </div>
               </Col>
             </Row>
           </Container>
